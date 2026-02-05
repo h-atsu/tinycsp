7c8f72e1228beeb54586efa03c281dc0526b8231
atsuhiro <jhuim08@gmail.com>
2026-02-06 01:02:24 +0900

    [add] made rust backend

--- python/tinycsp/tinycsp.py
+++ python/tinycsp/tinycsp.py
@@ -1,7 +1,8 @@
 from collections.abc import Callable
 from copy import deepcopy
-from typing import TypeAlias
+from typing import Literal, TypeAlias
 
+from tinycsp import _core
 from tinycsp.constraint import Constraint, EqualConstraint, NotEqualConstraint
 from tinycsp.domain import Domain
 from tinycsp.exceptions import Inconsistency
@@ -54,7 +55,35 @@ class TinyCSP:
     def first_not_fixed(self) -> Variable | None:
         return next((var for var in self.variables if not var.dom.is_fixed()), None)
 
-    def dfs(self, on_solution: OnSolutionCallback, *, stop_after_first: bool = False) -> bool:
+    def dfs(
+        self,
+        on_solution: OnSolutionCallback,
+        *,
+        stop_after_first: bool = False,
+        backend: Literal["python", "rust"] = "python",
+    ) -> bool:
+        if backend == "python":
+            return self.dfs_py(on_solution, stop_after_first=stop_after_first)
+        elif backend == "rust":
+            return self.dfs_rs(on_solution, stop_after_first=stop_after_first)
+        else:
+            raise ValueError(f"Unknown backend: {backend}")
+
+    def dfs_rs(
+        self, on_solution: OnSolutionCallback, *, stop_after_first: bool = False
+    ) -> bool:
+        ok, n_recur = _core.dfs_rs(
+            self.variables,
+            self.constraints,
+            on_solution,
+            stop_after_first,
+        )
+        self.n_recur = n_recur
+        return ok
+
+    def dfs_py(
+        self, on_solution: OnSolutionCallback, *, stop_after_first: bool = False
+    ) -> bool:
         self.n_recur += 1
 
         not_fixed_var = self.first_not_fixed()
